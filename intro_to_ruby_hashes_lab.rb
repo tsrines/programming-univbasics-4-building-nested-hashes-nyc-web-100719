83035e838da18dd4fbed7077bba61acbdb83bf4b
Timothy Rines <tsrines@gmail.com>
2019-09-22 18:49:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      :one_key
-	      :two_key
-	      :three_key
-	      :four_key
+	      :one_key => {},
+	      :two_key => {},
+	      :three_key => {},
+	      :four_key => {}
 	    }
 	  }
   }
