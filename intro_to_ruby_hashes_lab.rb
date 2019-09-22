6cd50488e8945313a850170b28c06ec5fd612c5f
Timothy Rines <tsrines@gmail.com>
2019-09-22 19:00:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,7 +46,8 @@ def monopoly_with_fourth_tier
 	      :three_pieces_owned => "100",
 	      :four_pieces_owned => "200"},
 	    :names => {
-	      :reading_railroad => 
+	      :reading_railroad => {}
+	      :pennsylvania_railroad => {}
 	    }
 	  }
   }
