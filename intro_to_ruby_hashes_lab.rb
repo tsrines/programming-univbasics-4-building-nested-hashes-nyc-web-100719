a136a5a5a0cfb71438d829ea8ea5aa6201c1b4cf
Timothy Rines <tsrines@gmail.com>
2019-09-22 19:09:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,16 @@ def monopoly_with_third_tier
 	      :three_pieces_owned => "100",
 	      :four_pieces_owned => "200"},
 	    :names => {
-	      :reading_railroad => {}
-	      :pennsylvania_railroad => {}
+	      :reading_railroad => 
+	      {
+	        
+	      },
+	      :pennsylvania_railroad => 
+	      {
+	        
+	      },
+	      :b_and_o
+
 	    }
 	  }
   }
